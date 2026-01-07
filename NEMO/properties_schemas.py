fb0ae6b28a6ba9fe7c93ad54a964b092fedd1630
李烨 <liye@liyedeMacBook-Pro.local>
2026-01-07 09:24:15 +0800

    同步最新代码

--- NEMO/properties_schemas.py
+++
@@ -1,6 +0,0 @@
-# This can be extended in settings.py
-TOOL_PROPERTIES_JSON_SCHEMA = {
-    "type": "object",
-    "properties": {},
-    "additionalProperties": True,
-}
--- NEMO/views/constants.py
+++
@@ -1,2 +0,0 @@
-# Kept for backward compatibility. remove in future version of NEMO
-from NEMO.constants import *
